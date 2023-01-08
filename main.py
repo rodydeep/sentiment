cffa6353f43a4b76796e197c8d55377a9cbbdf37
rodydeep <121086606+rodydeep@users.noreply.github.com>
2023-01-08 15:40:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-from sklearn.feature_extraction.text import CountVectorizer
 import seaborn as sns
 from textblob import TextBlob
 import re 
