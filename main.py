61c6d6b45c70ecc8409d3b333cc6aa9671bd0618
rodydeep <121086606+rodydeep@users.noreply.github.com>
2023-01-08 15:39:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
